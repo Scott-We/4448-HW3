b2749454fd5080f7712785bd2cd5ad6ce97e57c7
Scott Weygandt <scwe4009@colorado.edu>
2019-02-23 15:54:17 -0700

    started building framework for simulation, and adding methods

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,2 +1,15 @@
 public class Simulation {
+	Simulation(){
+	
+	}
+	boolean setup(){
+		return(true);
+	}
+	
+	boolean run(int numDays){
+		for(int i = 1; i <= numDays; i++){
+			System.out.println("Today is day: " + i);
+		}
+		return(true);
+	}
 }
