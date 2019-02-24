27b7e03380e6f1babe935cdaa17342f88f9cfdf1
JoelCourtney <joel.courtney@colorado.edu>
2019-02-23 17:59:24 -0700

    rental to string

--- src/Rental.java
+++ src/Rental.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Rental {
 	private ArrayList<Tool> tools = new ArrayList<Tool>();
 	int rentDate, returnDate;
+	String renter;
 
-	public Rental(int _rentDate, int _returnDate) {
+	public Rental(int _rentDate, int _returnDate, String _renter) {
 		rentDate = _rentDate;
 		returnDate = _returnDate;
+		renter = _renter;
 	}
 
 	public void addTool(Tool t) {
@@ -23,5 +25,15 @@ public class Rental {
 
 	public int numTools() {
 		return tools.size();
+	}
+
+	public String toString() {
+		String result = renter + " rented [";
+		result += tools.get(0).getID();
+		for (int i = 1; i < tools.size(); i++) {
+			result += "," + tools.get(i).getID();
+		}
+		result += "] from " + rentDate + " to " + returnDate;
+		return result;
 	}
 }
