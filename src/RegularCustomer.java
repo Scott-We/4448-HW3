bf174f1adda3b174b8e3cd86a1c44e37e193c44a
JohnO <joos5295@colorado.edu>
2019-02-23 17:01:21 -0700

    Customer abstract class

---
+++ src/RegularCustomer.java
@@ -0,0 +1,11 @@
+public class BusinessCustomer extends Customer
+{
+    public BusinessCustomer()
+    {
+        maxTools = 1;
+        minTools = 3
+        minDays = 3;
+        maxDays = 5;
+    }
+
+}
